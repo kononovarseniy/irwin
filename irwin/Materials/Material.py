f613448ebaf9ed9bd91489482625639e55841b8a
Arseniy Kononov <kononovarseniy@gmail.com>
2020-10-22 22:55:33 +0700

    Removed irwin.Material

--- irwin/Materials/Material.py
+++
@@ -1,3 +0,0 @@
-class Material:
-    def __init__(self):
-        pass
--- irwin/Materials/SiliconMaterial.py
+++
@@ -1,7 +0,0 @@
-from irwin.Materials.Material import Material
-
-
-class SiliconMaterial(Material):
-    def __init__(self):
-        super().__init__()
-        print(f'In ctor of Silicon Material')
