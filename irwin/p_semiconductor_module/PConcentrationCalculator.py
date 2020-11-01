14fb545025869a81543591dcd373434289c8a6f0
Pavel Ianko <yanpavevg@yandex.ru>
2020-11-01 19:19:38 +0700

    initial commit

--- irwin/p_semiconductor_module/PConcentrationCalculator.py
+++ irwin/p_semiconductor_module/PConcentrationCalculator.py
@@ -7,21 +7,19 @@ class PConcentrationCalculator(ConcentrationCalculator):
         self.temperature = None
         self.acceptor_energy = None
         self.donor_energy = None
+        self.material = None
 
     def __repr__(self):
         return f'T={self.temperature}, AcceptorE={self.acceptor_energy}, DonorE={self.donor_energy}'
 
     def calculate_concentration(self, *args, **kwargs):
-        for key, val in kwargs.items():
-            if key == 'temperature':
-                self.temperature = val
-            elif key == 'acceptor_energy':
-                self.acceptor_energy = val
-            elif key == 'donor_energy':
-                self.donor_energy = val
-            # elif key == 'material':
-            #    self.material = val
+        self.temperature = kwargs['temperature']
+        self.acceptor_energy = kwargs['acceptor_energy']
+        self.donor_energy = kwargs['donor_energy']
+        self.material = kwargs['material']
 
         # Вот теперь приступаем к алгоритму
         print(f'Calculator begins calc with parameters {self.__repr__()}')
+
+
         return
