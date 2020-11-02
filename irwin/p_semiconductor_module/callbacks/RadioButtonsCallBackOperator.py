08e2eadce73279cf6e82e16efd9c9f8268b7cd2a
Pavel Ianko <yanpavevg@yandex.ru>
2020-11-02 21:58:16 +0700

    added radiobuttons callbacks

---
+++ irwin/p_semiconductor_module/callbacks/RadioButtonsCallBackOperator.py
@@ -0,0 +1,24 @@
+from irwin.CalculationParameters import CalculationParameters
+from irwin.CallbackOperator import CallbackOperator
+
+
+
+class RadioButtonsCallBackOperator(CallbackOperator):
+    def __init__(self):
+        self.window = None
+        self.parameters = CalculationParameters()
+
+    def connect_callback(self, window):
+        self.window = window
+        window.ResistivityradioButton.clicked.connect(self.set_plotting_parameter)  # What to plot along Y-axis - resistivity or
+                                                                                    # Conductivity
+        window.ConductivityradioButton.clicked.connect(self.set_plotting_parameter)
+
+    def set_plotting_parameter(self):
+        if self.window.ResistivityradioButton.isChecked():
+            self.parameters.plot_resistivity = True
+            self.parameters.plot_conductivity = False
+        elif self.window.ConductivityradioButton.isChecked():
+            self.parameters.plot_conductivity = True
+            self.parameters.plot_resistivity = False
+
