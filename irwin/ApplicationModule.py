c241f4669c746b518c59579c94f8d4d0cf9af017
Pavel Ianko <yanpavevg@yandex.ru>
2020-11-14 18:09:54 +0700

    GUI interface update

--- irwin/ApplicationModule.py
+++ irwin/ApplicationModule.py
@@ -6,10 +6,12 @@ class ApplicationModule(ABC):
     def __init__(self):
         self.main_window = None  # a class inherited from QMainWindow
 
-    @abstractmethod
-    def run(self, window):
-        pass
+    def run(self, main_window):
+        self.main_window = main_window
+        self.connect_all_callbacks()
 
     def connect_all_callbacks(self):
         for conn in self.callback_operators:
             conn.connect_callback(self.main_window)  # TODO: код в родителя
+
+
