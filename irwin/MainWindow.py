e0e231bae3c4f6aded31308afdd5ea6e1c88cc67
ekalyanova <e_kalyanova.74@mail.ru>
2020-11-16 11:08:04 +0700

    draw_upds

--- irwin/MainWindow.py
+++ irwin/MainWindow.py
@@ -2,8 +2,15 @@ from PyQt5 import QtWidgets
 
 from irwin.design import Ui_MainWindow
 
+from irwin.p_semiconductor_module.PPlotCanvas import PPlotCanvas
+from irwin.n_semiconductor_module.NPlotCanvas import NPlotCanvas
+
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
+        self.pTypePlot = PPlotCanvas(parent=self)
+        self.pTypePlot.move(0, 0)
+        self.nTypePlot = NPlotCanvas(parent=self)
+        self.nTypePlot.move(0, 350)
         self.setupUi(self)
