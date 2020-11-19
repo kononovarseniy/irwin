26de23cc8f45aeb0da4b202d4c01ad1f79eb539e
Arseniy Kononov <kononovarseniy@gmail.com>
2020-11-19 11:37:13 +0700

    Updated window layout

--- irwin/MainWindow.py
+++ irwin/MainWindow.py
@@ -1,15 +1,9 @@
 from PyQt5 import QtWidgets
 
-from irwin.PlotCanvas import PlotCanvas
 from irwin.design import Ui_MainWindow
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        # TODO: Can we somehow automatically put it into design.py?
-        self.pTypePlot = PlotCanvas(parent=self)
-        self.pTypePlot.move(5, 5)
-        self.nTypePlot = PlotCanvas(parent=self)
-        self.nTypePlot.move(5, 365)
         self.setupUi(self)
