3a788fc1271a172caed0d5b216e8e9373fcb6b93
Arseniy Kononov <kononovarseniy@gmail.com>
2020-12-01 16:45:10 +0700

    Added 'show all parameters' checkbox

--- irwin/MainWindow.py
+++ irwin/MainWindow.py
@@ -7,3 +7,5 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.p_show_all.toggled.emit(False)
+        self.n_show_all.toggled.emit(False)
